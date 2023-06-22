280a8a24a431428ba4b409e7188a752ab4442f36
[dervieux.ben@gmail.com](mailto:dervieux.ben@gmail.com) <dervieux.ben@gmail.com>
2023-06-22 11:40:32 +0200

    update footer, creation contact page with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './Components/contact/contact.component';
+import { CalendarComponent } from './Components/calendar/calendar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CalendarComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './Components/calendar/calendar.component';
@@ -10,7 +9,9 @@ import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { FormRdvComponent } from './Components/formRdv/form-rdv/form-rdv.component';
 import { PopUpComponent } from './Components/pop-up/pop-up.component';
-
+import { ContactComponent } from './Components/contact/contact.component';
+import { LoginComponent } from './Components/login/login.component';
+import { FormLoginComponent } from './Components/form-login/form-login.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +21,11 @@ import { PopUpComponent } from './Components/pop-up/pop-up.component';
     CalendarComponent,
     FormRdvComponent,
     PopUpComponent,
+    ContactComponent,
+    LoginComponent,
+    FormLoginComponent,
   ],
-  imports: [
-    BrowserModule, 
-    AppRoutingModule, 
-    ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
